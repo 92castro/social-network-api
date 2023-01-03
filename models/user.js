03123acde59872e1eda71c3602d9f991cfb078a4
92castro <celin92castro@gmail.com>
2023-01-03 16:53:15 -0700

    create folders and models schema

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+var validator = require("validator");
+
+// Schema to create user model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        isAsync: false,
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends;
+});
+
+const user = model("user", userSchema);
+
+module.exports = user;
